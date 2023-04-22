1fdcb57bb2a93e9cca92d03ac2b68365b461bc10
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-04-22 15:41:17 -0600

    feature - add READ MORE links inside cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -78,6 +78,11 @@ export const Card = ({ card, answered, setAnswered, correct, setCorrect }) => {
     createAnswersList()
   }, [data])
 
+  const generateHTMLLink = () => {
+    const symbolLink = card.symbol.toLowerCase().replace(" ", "-")
+    return `https://www.adl.org/resources/hate-symbol/${symbolLink}`
+  }
+
   return (
     <main
       style={{ transform: `translateX(-${carouselIndex * 100}%` }}
@@ -91,10 +96,10 @@ export const Card = ({ card, answered, setAnswered, correct, setCorrect }) => {
           </div>
         </div>
         <div className="back">
-          <p className="card-description">
+          <span className="card-description">
             {card.desc}
             <div className="scroll-shadow" />
-          </p>
+          </span>
         </div>
       </section>
       {/* card options */}
@@ -114,9 +119,16 @@ export const Card = ({ card, answered, setAnswered, correct, setCorrect }) => {
           </div>
         </div>
         {flipped ? (
-          <button className="menu-button" onClick={advanceCard}>
-            Next
-          </button>
+          <>
+            <button className="menu-button" onClick={advanceCard}>
+              Next
+            </button>
+            <button className="menu-button">
+              <a target="_blank" href={generateHTMLLink()}>
+                Read More
+              </a>
+            </button>
+          </>
         ) : (
           <div className="card-options">{createAnswerButtons()}</div>
         )}
