23324fabbb010efc634fceffa117551ab862d536
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2023-04-27 12:49:29 -0600

    add IF logo, email, favicon

--- src/About/About.js
+++ src/About/About.js
@@ -20,18 +20,36 @@ export const About = () => {
         </p>
         <p>
           As a patriotic antifascist, I feel it is my duty to educate and
-          inspire my country to recognize the threat posed by fascist & white
-          supremacist ideologies and organizations. Use this app as a tool to
-          quickly recognize the signs & symbols of these increasingly bold
-          hate-groups. You may be unpleasantly surprised how many are present in
-          plain view on any given day.
+          inspire my country to recognize the existential threat posed by
+          fascist & white supremacist ideologies & organizations. Use this app
+          as a tool to quickly recognize the signs & symbols of these
+          increasingly bold hate-groups. You may be unpleasantly surprised how
+          many are present in plain view on any given day.
         </p>
         <p>
           Unfortunately the data from ADL is a little outdated and far from
           complete. This list does not contain any references to Q-Anon, Stop
           The Steal, or other MAGA conspiracies; nor does it mention Proud Boys,
-          3%ers, Patriot Prayer or any modern militia movements. Care must be
-          taken to quantify and label these organizations.
+          3%-ers, Patriot Prayer or any modern militia movements. Care must be
+          taken to quantify, label, and -of course- fight these organizations.
+        </p>
+        <p className="center">
+          This app was made by <a href="https://catalyst.sex/">Catalyst</a>.
+        </p>
+        <p className="center">
+          You can read about it's creation on{" "}
+          <a href="https://github.com/Catalyst4Change/fash-cards-rebuild-v1">
+            GitHub
+          </a>
+          .
+        </p>
+        <p className="center">
+          Please direct all comments, suggestions, <br />
+          and death-threats to:
+          <br />{" "}
+          <a href="mailto:catalystfourchange@icloud.com?subject=Fash Cards">
+            CatalystFourChange@iCloud.com
+          </a>
         </p>
       </div>
       <Link to="/">
